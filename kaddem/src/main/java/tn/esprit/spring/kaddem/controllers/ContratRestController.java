cd8425f5b56b80e910459ee6bb5ac5ac54293cc1
Amani BEN SASSI <123634459+amanibensassi@users.noreply.github.com>
2024-11-03 11:13:41 +0100

    Update ContratRestController.java

--- kaddem/src/main/java/tn/esprit/spring/kaddem/controllers/ContratRestController.java
+++ kaddem/src/main/java/tn/esprit/spring/kaddem/controllers/ContratRestController.java
@@ -11,7 +11,7 @@ import tn.esprit.spring.kaddem.services.IContratService;
 
 import java.util.Date;
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/contrat")
