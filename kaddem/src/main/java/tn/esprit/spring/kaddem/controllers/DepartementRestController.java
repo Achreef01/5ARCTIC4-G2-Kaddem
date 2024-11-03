32b059e0a5be83a1eb4835a479446b3024385804
Amani BEN SASSI <123634459+amanibensassi@users.noreply.github.com>
2024-11-03 11:13:53 +0100

    Update DepartementRestController.java

--- kaddem/src/main/java/tn/esprit/spring/kaddem/controllers/DepartementRestController.java
+++ kaddem/src/main/java/tn/esprit/spring/kaddem/controllers/DepartementRestController.java
@@ -6,7 +6,7 @@ import tn.esprit.spring.kaddem.entities.Departement;
 import tn.esprit.spring.kaddem.services.IDepartementService;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/departement")
