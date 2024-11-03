6fed09605b2a4f57683ac999579a40fd79cadfaf
Achref <achref.ochi@esprit.tn>
2024-11-03 22:01:02 +0100

    Update on Backend

--- kaddem/src/main/java/tn/esprit/spring/kaddem/configurations/WebConfig.java
+++ kaddem/src/main/java/tn/esprit/spring/kaddem/configurations/WebConfig.java
@@ -10,11 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow all paths
-                .allowedOrigins("http://192.168.33.10:4200") // Specify allowed origins
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // Allow credentials (like cookies) if needed
-                // NOSONAR
+        registry.addMapping("/**")
+                .allowedOrigins("http://192.168.33.10:4200") // NOSONAR
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
     }
 }
