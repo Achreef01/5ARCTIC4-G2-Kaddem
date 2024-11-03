c57468cdce995ebf58f549f8c40360d36f6cdfc5
Hamma-Mrabet <mohamed.mrabet@esprit.tn>
2024-11-03 20:42:44 +0100

    Update WebConfig.java

--- kaddem/src/main/java/tn/esprit/spring/kaddem/configurations/WebConfig.java
+++ kaddem/src/main/java/tn/esprit/spring/kaddem/configurations/WebConfig.java
@@ -2,10 +2,13 @@ package tn.esprit.spring.kaddem.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@CrossOrigin(origins = "http://localhost:4200")
+
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -15,5 +18,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials (like cookies) if needed
+
     }
 }
