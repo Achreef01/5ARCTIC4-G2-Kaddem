2d07224e1b0bba9225ae9f8b7c3928e693878a82
Achref <achref.ochi@esprit.tn>
2024-11-03 03:04:14 +0100

    Test for Deployment

--- kaddem/src/main/java/tn/esprit/spring/kaddem/configurations/WebConfig.java
+++ kaddem/src/main/java/tn/esprit/spring/kaddem/configurations/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all paths
-                .allowedOrigins("http://192.168.33.10:4200", "http://localhost:4200") // Specify allowed origins
+                .allowedOrigins("http://192.168.33.10:4200", "http://localhost:4200", "*") // Specify allowed origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials (like cookies) if needed
