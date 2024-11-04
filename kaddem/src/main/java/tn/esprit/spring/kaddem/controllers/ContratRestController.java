3c4ea2d208816bb5ff21e88557b0fce4ce9c6514
Hamma-Mrabet <mohamed.mrabet@esprit.tn>
2024-11-04 20:33:18 +0100

    Update ContratRestController.java

--- kaddem/src/main/java/tn/esprit/spring/kaddem/controllers/ContratRestController.java
+++ kaddem/src/main/java/tn/esprit/spring/kaddem/controllers/ContratRestController.java
@@ -1,10 +1,12 @@
 package tn.esprit.spring.kaddem.controllers;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.kaddem.entities.Contrat;
+import tn.esprit.spring.kaddem.services.ContratServiceImpl;
 import tn.esprit.spring.kaddem.services.IContratService;
 
 import java.util.Date;
@@ -12,56 +14,66 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@CrossOrigin(origins = "http://192.168.28.144:4200", maxAge = 3600, allowCredentials = "true") // NOSONAR
 @RequestMapping("/contrat")
+@CrossOrigin(origins = "http://192.168.28.144:4200", maxAge = 3600, allowCredentials = "true") // NOSONAR
 public class ContratRestController {
 	IContratService contratService;
-
+	// http://localhost:8089/Kaddem/contrat/retrieve-all-contrats
 	@GetMapping("/retrieve-all-contrats")
 	public List<Contrat> getContrats() {
-
-		return contratService.retrieveAllContrats();
+		List<Contrat> listContrats = contratService.retrieveAllContrats();
+		return listContrats;
 	}
-
+	// http://localhost:8089/Kaddem/contrat/retrieve-contrat/8
 	@GetMapping("/retrieve-contrat/{contrat-id}")
 	public Contrat retrieveContrat(@PathVariable("contrat-id") Integer contratId) {
 		return contratService.retrieveContrat(contratId);
 	}
 
+	// http://localhost:8089/Kaddem/econtrat/add-contrat
 	@PostMapping("/add-contrat")
 	public Contrat addContrat(@RequestBody Contrat c) {
 		Contrat contrat = contratService.addContrat(c);
 		return contrat;
 	}
 
+	// http://localhost:8089/Kaddem/contrat/remove-contrat/1
 	@DeleteMapping("/remove-contrat/{contrat-id}")
 	public void removeContrat(@PathVariable("contrat-id") Integer contratId) {
 		contratService.removeContrat(contratId);
 	}
 
+	// http://localhost:8089/Kaddem/contrat/update-contrat
 	@PutMapping("/update-contrat")
 	public Contrat updateContrat(@RequestBody Contrat c) {
-
-		return contratService.updateContrat(c);
+		Contrat contrat= contratService.updateContrat(c);
+		return contrat;
 	}
 
-
+		/*@PutMapping(value = "/assignContratToEtudiant/{ce}/{nomE}/{prenomE}")
+		public Contrat assignContratToEtudiant (Contrat ce, String nomE, String prenomE){
+		return 	(contratService.affectContratToEtudiant(ce, nomE, prenomE));
+		}*/
 
 	@PutMapping(value = "/assignContratToEtudiant/{idContrat}/{nomE}/{prenomE}")
 	public Contrat assignContratToEtudiant (Integer idContrat, String nomE, String prenomE){
+		//	Contrat c= contratService.affectContratToEtudiant()
 		return 	(contratService.affectContratToEtudiant(idContrat, nomE, prenomE));
 	}
 
-		@GetMapping(value = "/getnbContratsValides/{startDate}/{endDate}")
-		public Integer getnbContratsValides(@PathVariable(name = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
-										  @PathVariable(name = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate) {
+	//The most common ISO Date Format yyyy-MM-dd — for example, "2000-10-31".
+	@GetMapping(value = "/getnbContratsValides/{startDate}/{endDate}")
+	public Integer getnbContratsValides(@PathVariable(name = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
+										@PathVariable(name = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate) {
 
-			return contratService.nbContratsValides(startDate, endDate);
-		}
+		return contratService.nbContratsValides(startDate, endDate);
+	}
 
-    @Scheduled(cron="0 0 13 * * *")//(cron="0 0 13 * * ?")(fixedRate =21600)
+	//Only no-arg methods may be annotated with @Scheduled
+	@Scheduled(cron="0 0 13 * * *")//(cron="0 0 13 * * ?")(fixedRate =21600)
 	@PutMapping(value = "/majStatusContrat")
 	public void majStatusContrat (){
+		//return 	(contratService.affectContratToEtudiant(ce, nomE, prenomE));
 		contratService.retrieveAndUpdateStatusContrat();
 
 	}
@@ -71,7 +83,7 @@ public class ContratRestController {
 	@GetMapping("/calculChiffreAffaireEntreDeuxDate/{startDate}/{endDate}")
 	@ResponseBody
 	public float calculChiffreAffaireEntreDeuxDates(@PathVariable(name = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
-	@PathVariable(name = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate) {
+													@PathVariable(name = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate) {
 
 		return contratService.getChiffreAffaireEntreDeuxDates(startDate, endDate);
 	}
